ce606d2a174257b637551a701c0efba41c710711
azm819 <37504894+azm819@users.noreply.github.com>
2019-06-12 16:53:37 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,48 +1,38 @@
 #include "utils.h"
 
-using namespace std;
-
 int main() {
     int sockfd = createSocket();
 
-    auto my_addr = genStructFields("./socket");
+    if (unlink(socketName) == -1 && errno != ENOENT)
+        printError("unlink");
 
-    if (unlink("./socket") == -1) {
-        perror("unlink");
-        exit(EXIT_FAILURE);
-    }
+    auto address = genAddrStruct();
 
-    if (bind(sockfd, (struct sockaddr *) &my_addr.second, my_addr.first) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
+    if (bind(sockfd, (struct sockaddr*)&address, sizeof(struct sockaddr_un)) == -1)
+        printError("bind");
 
     int backlog = SOMAXCONN;
-    if (listen(sockfd, backlog) == -1) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    if (listen(sockfd, backlog) == -1)
+        printError("listen");
 
-    struct sockaddr_in addr;
-    int clientSockd;
-    socklen_t addrlen;
-    while (true) {
-        if ((clientSockd = accept(sockfd, (sockaddr *) &addr, &addrlen)) == -1) {
-            perror("accept");
-            break;
-        }
+    SERVER_INFO << "Server running.\n";
+    int clientSocket;
+    while ((clientSocket = accept(sockfd, nullptr, nullptr)) != -1) {
+        auto msg = genMsgStruct();
 
-        auto message = genMsgFields();
-
-        struct cmsghdr *cmsg = CMSG_FIRSTHDR(&message);
-        cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+        struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
         cmsg->cmsg_level = SOL_SOCKET;
         cmsg->cmsg_type = SCM_RIGHTS;
+        cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+        int* fd = (int*)CMSG_DATA(cmsg);
+        *fd = 1;
 
-        if (sendmsg(clientSockd, &message, 0) == -1) {
-            perror("sendmsg");
-            exit(EXIT_FAILURE);
-        }
+        if(sendmsg(clientSocket, &msg, 0) == -1)
+            printError("sendmsg");
+
+        SERVER_INFO << "New user.\n";
     }
-    return 0;
+
+    std::cerr << "Connetcion failed.\n";
+    exit(EXIT_FAILURE);
 }
